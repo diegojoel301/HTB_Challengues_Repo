4d53235b5b015497b190ec11e57e53bc9ef642c8
diegojoel301 <diegojoelcondoriquispe@gmail.com>
2025-01-05 16:43:21 +0100

    Nuevo commit

---
+++ web_pop_restaurant/challenge/Models/IceCreamModel.php
@@ -0,0 +1,14 @@
+<?php
+	class IceCream
+	{
+		public $flavors;
+		public $topping;
+
+		public function __invoke()
+		{
+			
+			foreach ($this->flavors as $flavor) {
+				echo $flavor."\n";
+			}
+		}
+	}
